9bd5445a594093deb8f5d29b9970e72e74103dc4
shreyansh <viragi311@gmail.com>
2019-08-03 17:40:29 -0700

    added function to handle click

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,9 +4,27 @@ import {connect} from 'react-redux';
 
 class Board extends React.Component{
     render(){
+       // console.log(this.props);
+        let elem = this.props.board.map((item,i)=>{
+            return(
+                <div className="col">
+                    {item.map((cell,rowI)=>{
+                        return(
+                            <div className="cell">
+                                {cell==null ? <div className="sign" onClick={()=>this.props.handleClick(i,rowI,this.props.currentPlayer)}></div>
+                                            : <div className="sign"> {cell}</div> }
+                                
+                            </div>
+                        )
+                    })}
+                </div>    
+            )
+
+        })
+
         return(
-            <div>
-                Hello Board here
+            <div className="container">
+                {elem}
             </div>
         )
     }
@@ -15,17 +33,21 @@ class Board extends React.Component{
 
 function mapDispatchToProps(dispatch){
     return{
-        handleClick : dispatch({
-            type:"click"
-        })
+        handleClick : (i,j,player)=>{dispatch({
+            type:"click",
+            col:i,
+            row:j,
+            player:player
+        })}
     }
 }
 
 
 function mapStateToProps(state){
     return{
-        data: state.data
+        board: state.board,
+        currentPlayer:state.currentPlayer//P1
     }
-}
+}   
 
 export default connect(mapStateToProps,mapDispatchToProps)(Board);
--- src/reducer/rootReducer.js
+++ src/reducer/rootReducer.js
@@ -1,10 +1,17 @@
-let initialState=[];
+let initialState={
+    board:[[null,null,null],[null,null,null],[null,null,null]],
+    P1:"O",
+    P2:"X",
+    currentPlayer:"P1"
+};
 
 let rootReducer=(state=initialState, action)=>{
     if(action.type=="click"){
-        let newstate=["demo"];
-        console.log("clicked");
-        return newstate;
+        let newBoard= [...state.board];
+        newBoard[action.col] =[...newBoard[action.col]];
+        newBoard[action.col][action.row]= state[action.player];
+        let newPlayer = state[action.player] == "O" ? "P2" : "P1";
+        return {...state,board:newBoard,currentPlayer:newPlayer};
         
     }else{
         return state;
